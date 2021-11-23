72ca4691fd71029ea8d4cea0325cd1e1974d2d5f
victormichelsilva <victormichelsilva@gmail.com>
2021-11-23 09:56:43 -0300

    Adding files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Abstracao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servicoEmail = new ServicoEmail();
+
+            servicoEmail.EnviarEmail();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ServicoEmail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstracao1
+{
+    public class ServicoEmail
+    {
+        public void EnviarEmail() 
+        {
+            Conectar();
+            Autenticar();
+            Console.WriteLine("Enviando o e-mail");
+            Desconectar();
+        }
+
+        private void Desconectar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Autenticar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Conectar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
